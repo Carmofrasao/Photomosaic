820a9937c268d00d82f662f2ba1f6a8abd241d66
Anderson Frasao <anderfrasao52@gmail.com>
2024-02-15 22:57:47 -0300

    add wasm

--- App.js
+++ App.js
@@ -2,12 +2,13 @@
 function gerarFotomosaico() {
     var caminhoImagemOriginal = document.getElementById("inputImagemOriginal").value;
     var diretorioSelecionado = document.getElementById("selectDiretorioImagens").value;
-
+    console.log("chegou aqui");
     // Verifique se os caminhos dos arquivos foram fornecidos
     if (caminhoImagemOriginal && caminhoConjuntoImagens) {
         // Chame a função do módulo WebAssembly para gerar o fotomosaico
+        console.log("chegou aqui 1");
         Module.ccall('main', 'number', ['string', 'string'], ['./'+caminhoImagemOriginal, caminhoConjuntoImagens]);
-        
+        console.log("chegou aqui 2");
         // Uma vez que a função é chamada, você pode querer exibir o resultado na tela ou fazer outra coisa com ele
         // Por exemplo, você pode atualizar uma imagem na página com o resultado gerado
     } else {
