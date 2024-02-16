a4bfe6fd4a33c3549a6df044ac63746446b4390a
Anderson Frasao <anderfrasao52@gmail.com>
2024-02-15 22:38:50 -0300

    add wasm

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ function gerarFotomosaico() {
     // Verifique se os caminhos dos arquivos foram fornecidos
     if (caminhoImagemOriginal && caminhoConjuntoImagens) {
         // Chame a função do módulo WebAssembly para gerar o fotomosaico
-        Module.ccall('generatePhotomosaic', 'number', ['string', 'string'], ['./'+caminhoImagemOriginal, caminhoConjuntoImagens]);
+        Module.ccall('main', 'number', ['string', 'string'], ['./'+caminhoImagemOriginal, caminhoConjuntoImagens]);
         
         // Uma vez que a função é chamada, você pode querer exibir o resultado na tela ou fazer outra coisa com ele
         // Por exemplo, você pode atualizar uma imagem na página com o resultado gerado
